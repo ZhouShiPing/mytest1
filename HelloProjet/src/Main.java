e69610b5f091398d2bf6044a9e3f8f57704d6f98
ZhouShiPing <2463477903@qq.com>
2020-04-24 11:21:32 +0800

    hello git

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,9 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
+
+    @Test
+    public void testGit() {
+        System.out.println("Hello Git!");
+    }
 }
